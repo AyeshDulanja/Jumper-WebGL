306d2c8e55d463136bd92e857ed33732c0765b3a
Ayesh Dulanja <ayeshdulanja@gmail.com>
2022-07-26 16:03:22 +0530

    Update game.js

--- game.js
+++ game.js
@@ -75,5 +75,30 @@ function create ()
 }
 
 function update(){
-    
+    //This populates the cursors object with four properties: up, down, left, right, that are all instances of Key objects
+    cursors = this.input.keyboard.createCursorKeys();
+
+    if (cursors.left.isDown)
+    {
+        player.setVelocityX(-160);
+
+        player.anims.play('left', true);
+    }
+    else if (cursors.right.isDown)
+    {
+        player.setVelocityX(160);
+
+        player.anims.play('right', true);
+    }
+    else
+    {
+        player.setVelocityX(0);
+
+        player.anims.play('turn');
+    }
+
+    if (cursors.up.isDown && player.body.touching.down)
+    {
+        player.setVelocityY(-330);
+    }
 }
